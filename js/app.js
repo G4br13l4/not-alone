1ab2728df0d7fbbc2c2a5573ced12687ded91675
G4br13l4 <gabolf93@gmail.com>
2018-01-29 21:36:17 -0600

    autenticacion con Google

--- js/app.js
+++ js/app.js
@@ -8,41 +8,73 @@
     messagingSenderId: "971433045993"
   };
   firebase.initializeApp(config);
+//Autenticacion con Google
 
-  var bigOne = document.getElementById("bigOne");
-  var ref = firebase.database().ref().child("text");
-  ref.on("value", snap => bigOne.innerText = snap.val());
+  $("#googleBtn").click(function(){
+    authGoogle();
+  })
+
+  function authGoogle() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    authentication(provider);
+  }
+
+  function authentication(){
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+};
 
 
-// Funcionalidad 
+// Funcion para compartir un post en timeline
 $(document).ready(function(){
-  $("#share-btn").click(shareStory);
+$("#share-btn").click(shareStory);
+
 
   function shareStory (){
+
+      /*Creando elementos contenedores y agregandoles una clase*/
     var $post = $('<div />').addClass('row post');
     var $imgProfileBox = $('<div />').addClass('col-2');
     var $imgProfile = $("<img/>");
     var $textPostBox = $('<div />').addClass('col-10');
-    var $userName = $('<p/>').addClass('col-10');
-    posts-timeline
+    var $userName = $('<p/>');
+    var $postContent = $('<p/>');
+    
+
+      /*Agregando texto a elementos creados o cambiando atributos*/
+    $postContent.text($("textarea").val());
+
+      /*Linkeando elementos entre sí*/
+    $imgProfileBox.append($imgProfile);
+    $textPostBox.append($userName);
+    $textPostBox.append($postContent);
+    $post.append($imgProfileBox);
+    $post.append($textPostBox);
+    $('.posts-timeline').append($post);
+
+      /*Limpiando caja*/
+    $("textarea" ).val("");
   }
+  
 });
 
-/*<div class="row post">
-                    <div class="col-2">
-                        <img src="" alt="img-user">
-                    </div>
-                    <div class="col-10">
-                        <p>User Name</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt eius similique, dolor ut sapiente!</p>           
-                    </div>
-                </div>
-*/
 
 
-
-
-  // Splash view
+ // Splash view
   $(document).ready(function() {
     setTimeout(function() {
         $(".container-1").fadeOut(1500);

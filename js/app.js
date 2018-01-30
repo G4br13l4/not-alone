b398c25222f6a6271edcea42bcc6ef66c5bb01ae
G4br13l4 <gabolf93@gmail.com>
2018-01-29 21:56:44 -0600

    ajustes a autenticacion

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@
     authentication(provider);
   }
 
-  function authentication(){
+  function authentication(provider){
     firebase.auth().signInWithPopup(provider).then(function(result) {
   // This gives you a Google Access Token. You can use it to access the Google API.
   var token = result.credential.accessToken;
